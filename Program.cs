fa782e1fc8a841be1ab6ed7ef75561273e991e27
Abdellah <abdellahchbib20@gmail.com>
2025-06-17 21:23:22 +0100

    adding DbContext in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using GisServerProject.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Replace 'YourDbContext' with the actual name of your DbContext class
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        x => x.UseNetTopologySuite() // Enable spatial support
+    )
+);
 
 var app = builder.Build();
 
